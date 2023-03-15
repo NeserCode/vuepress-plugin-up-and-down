4fcbf2a534fb156c539a3981debf95d357645030
✨NeserCode <nesercode@gmail.com>
2023-03-15 14:48:19 +0800

    update plugin

---
+++ lib/client/components/UpAndDown.d.ts
@@ -0,0 +1,25 @@
+import "../styles/vars.css"
+import "../styles/up-and-down.css"
+export declare const UpAndDown: import("vue").DefineComponent<
+	{},
+	() => import("vue").VNode<
+		import("vue").RendererNode,
+		import("vue").RendererElement,
+		{
+			[key: string]: any
+		}
+	>,
+	{},
+	{},
+	{},
+	import("vue").ComponentOptionsMixin,
+	import("vue").ComponentOptionsMixin,
+	{},
+	string,
+	import("vue").VNodeProps &
+		import("vue").AllowedComponentProps &
+		import("vue").ComponentCustomProps,
+	Readonly<import("vue").ExtractPropTypes<{}>>,
+	{}
+>
+export default UpAndDown
